b6876adfca39c21ea9c5889e38273aef9399c39a
Fachry27 <fachry2710@gmail.com>
2023-03-13 16:29:37 +0700

    Golang With Mysql

---
+++ cmd/main/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/fachryotosaka/gowbook/pkg/routes"
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"log"
+	"net/http"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookstoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:8000", r))
+}
---
+++ pkg/config/app.go
@@ -0,0 +1,23 @@
+package config
+
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+var (
+	db *gorm.DB
+)
+
+func Connect() {
+	d, err := gorm.Open("mysql", "otosakalexa:fachry2710@tcp(127.0.0.1:3306)/gowbook?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		panic(err)
+	}
+
+	db = d
+}
+
+func GetDB() *gorm.DB {
+	return db
+}
